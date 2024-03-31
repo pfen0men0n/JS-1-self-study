0c4b1a78890ccc574c27de7b14f015a6c5dca402
unknown <msijkeee@gmail.com>
2024-03-31 16:20:18 +0700

    hw1

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+const checkStringLength = function (string, maxLength) {
+    if (string.length <= maxLength) {
+        return true;
+    }
+        return false;
+}
+console.log(checkStringLength('Привет как дела собака ты сутулая?', 50));
+
+const isPalindrome = function (string) {
+striing = string.toLowerCase().replace(/[^a-z]+/g,"");
+return string === string.split("").reverse().join("");
+
+}
+console.log(isPalindrome('топот'));
