ba4e76ebd9ebc45ecf33d332edbce5328f2f85ab
Лев Елистратов <msijkeee@gmail.com>
2024-06-23 19:54:07 +0700

    rendering thumbnails pictures of other users

---
+++ js/thumbnails.js
@@ -0,0 +1,20 @@
+const picturesContainer = document.querySelector('.pictures');
+picturesContainer.classList.remove('visually-hidden');
+const picturesTemlpate = document.querySelector('#picture').content.querySelector('.picture');
+
+const renderThumbnails = (drawPhotos) => {
+    const picturesContainerFragment = document.createDocumentFragment();
+    drawPhotos().forEach(({url, description, likes, comments})=> {
+    const userPhotoElement = picturesTemlpate.cloneNode(true);
+    userPhotoElement.querySelector('.picture__img').src = url;
+    userPhotoElement.querySelector('.picture__img').alt = description;
+    userPhotoElement.querySelector('.picture__likes').textContent = likes;
+    userPhotoElement.querySelector('.picture__comments').textContent = comments.length;
+
+    picturesContainerFragment.append(userPhotoElement);
+
+});
+
+    picturesContainer.append(picturesContainerFragment);
+};
+export {renderThumbnails};
