0ecb47adb6fc257c18e078e2653c504923aeeb69
unknown <msijkeee@gmail.com>
2024-07-29 20:16:33 +0700

    adding a pristine validatoin and listeners

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {createPhotoObjects} from './utils.js';
 import {renderThumbnails} from './thumbnails.js';
+import {addValidationAndListeners} from './form.js';
 
 renderThumbnails(createPhotoObjects);
-
+addValidationAndListeners();
 
